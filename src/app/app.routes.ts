52bc1d2a9f462bba9ed7b70a2161dd192ce3878e
amal velloth <amalv414@gmail.com>
2025-01-17 15:38:43 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Import your components
+import { LoginPageComponent } from './login/login.component';
+import { EmployeeListPageComponent } from './employee-list-page/employee-list-page.component';
+import { AddEmployeePageComponent } from './add-employee-page/add-employee-page.component';
+
+const routes: Routes = [
+  // Define routes with path and corresponding component
+  { path: '', redirectTo: '/login', pathMatch: 'full' },  // Default route
+  { path: 'login', component: LoginPageComponent },
+  { path: 'employee-list', component: EmployeeListPageComponent },
+  { path: 'add-employee', component: AddEmployeePageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], // Register routes
+  exports: [RouterModule]  // Export RouterModule for use in other modules
+})
+export class AppRoutingModule { }
